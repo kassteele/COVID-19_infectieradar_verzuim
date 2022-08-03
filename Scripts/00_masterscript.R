cb4345166039ab33be685bce1affda22411bd1b0
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2022-08-03 09:26:01 +0200

    Update masterscript

--- Scripts/00_masterscript.R
+++ Scripts/00_masterscript.R
@@ -1,6 +1,8 @@
 #
-# Verzuim per bedrijfstak
-# Relatie met dagelijkse infectieradar % klachten
+# Masterscript verzuim per bedrijfstak
+# Auteur: Jan van de Kassteele
+# i.s.m. Hester Korthals Altes en Jacco Wallinga
+# RIVM
 #
 
 source(file = "Scripts/01_initialiseren.R")
@@ -8,9 +10,5 @@ source(file = "Scripts/02_importeer_data.R")
 source(file = "Scripts/03_opschonen_data_symptomen.R")
 source(file = "Scripts/04_opschonen_data_verzuim.R")
 source(file = "Scripts/05_fit_modellen.R")
-source(file = "Scripts/06a_voorspel_fit.R")
-source(file = "Scripts/06b_voorspel_tijdreeks.R")
-source(file = "Scripts/07a_visualiseer_fit.R")
-source(file = "Scripts/07b_visualiseer_tijdreeks_fit.R")
-source(file = "Scripts/07c_visualiseer_tijdreeks_verzuim.R")
-source(file = "Scripts/08_exporteren_figuren.R")
+source(file = "Scripts/06_visualiseer_tijdreeks_fit.R")
+source(file = "Scripts/07_exporteer_figuren.R")
