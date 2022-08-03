9f166fe0f8ead0b7d26cb77318d53d31c84c9ea5
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2022-08-03 09:26:27 +0200

    Update init

--- Scripts/01_initialiseren.R
+++ Scripts/01_initialiseren.R
@@ -7,7 +7,30 @@ library(tidyverse)
 library(lubridate)
 library(ISOweek)
 library(readxl)
-library(mgcv)
+library(parallel)
+library(splines)
+library(colorspace)
 
-# Weeks start on Monday
-options(lubridate.week.start = 1)
+# Opties:
+# - Aantal cores voor parallel rekenen
+# - Weken starten op maandag
+options(
+  mc.cores = detectCores(),
+  lubridate.week.start = 1)
+
+# Seizoensperiode in weken
+p_seizoen <- 365.25/7
+
+# Functie om coefs uit een modelfit te halen o.b.v. naam patroon
+extract_coef <- function(object, pattern) {
+  coefs <- object %>% coef()
+  names_coefs <- coefs %>% names()
+  coefs[names_coefs %>% str_detect(pattern)]
+}
+
+# Functie om model matrix uit een modelfit te halen o.b.v. naam patroon
+extract_model_matrix <- function(object, pattern) {
+  model_matrix <- object %>% model.matrix()
+  names_coefs <- model_matrix %>% colnames()
+  model_matrix[, names_coefs %>% str_detect(pattern)] %>% matrix(nrow = nrow(model_matrix))
+}
