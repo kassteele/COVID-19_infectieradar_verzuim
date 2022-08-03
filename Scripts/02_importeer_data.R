a4de69c3ad0325e895bb86691b441b28d20eca90
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2022-08-03 09:27:27 +0200

    Infectieradardata nu afkomstig van R-schijf. Update SBI data

--- Scripts/02_importeer_data.R
+++ Scripts/02_importeer_data.R
@@ -3,14 +3,14 @@
 #
 
 # Importeer Infectieradar symptomen
-data_symptomen_org <- read_delim(
-  file = "https://data.rivm.nl/covid-19/COVID-19_Infectieradar_symptomen_per_dag.csv",
-  delim = ";",
+# Bron: R:\Projecten\COVID-19\Surveillance\Output\Infectieradar\Onderliggende_data\Percentage_klachten
+data_symptomen_org <- read_csv(
+  file = "Data/Percentage_klachten_over_tijd_20220719_0906.csv",
   show_col_types = FALSE)
 
 # Importeer verzuim data
 data_verzuim_org <- read_excel(
-  path = "Data/20220621_MF_NVP_GEM_per_week_en_SBI.xlsx",
+  path = "Data/20220721_MF_NVP_GEM_per_week_en_SBI.xlsx",
   sheet = 2,
   skip = 1,
   col_types = "text")
