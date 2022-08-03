582027f91ae08312ecb9ffb56fbfb02307df3e38
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2022-08-03 09:30:00 +0200

    png 1920 x 1080 pixels

--- Scripts/07_exporteer_figuren.R
+++ Scripts/07_exporteer_figuren.R
@@ -3,16 +3,6 @@
 #
 
 ggsave(
-  plot = plot_verzuim_fit,
-  filename = str_glue("Resultaten/Ziekteverzuim_bedrijfstak_fit_tm{max(data_verzuim$Week)}.png"),
-  width = 1280, height = 720, units = "px", dpi = 100)
-
-ggsave(
   plot = plot_verzuim_tijdreeks_fit,
   filename = str_glue("Resultaten/Ziekteverzuim_bedrijfstak_tijdreeks_fit_tm{max(data_verzuim$Week)}.png"),
-  width = 1280, height = 720, units = "px", dpi = 100)
-
-ggsave(
-  plot = plot_verzuim_tijdreeks,
-  filename = str_glue("Resultaten/Ziekteverzuim_bedrijfstak_tijdreeks_tm{max(data_verzuim$Week)}.png"),
-  width = 1280, height = 720, units = "px", dpi = 100)
+  width = 1920, height = 1080, units = "px", dpi = 100, bg = "white")
