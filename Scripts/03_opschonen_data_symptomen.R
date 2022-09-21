22f1a3c146dca91fa103bc38857edce911ffc24c
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2022-09-21 09:57:30 +0200

    Geen teller-noemer data meer, maar percentages uit open data

--- Scripts/03_opschonen_data_symptomen.R
+++ Scripts/03_opschonen_data_symptomen.R
@@ -4,12 +4,24 @@
 
 data_symptomen <- data_symptomen_org %>%
   mutate(
-    Week = timestamp.weekly %>%
-      floor_date(unit = "week")) %>%
+    # Week waarvoor Infectieradar data geldig is
+    Week = Date_of_statistics %>%
+      floor_date(unit = "week"),
+    # Een aantal datum hebben NA, bijvoorbeeld door een technische storing
+    # We imputeren Perc_covid_symptoms voor deze datums
+    # We noemen de nieuwe variabele Perc_symptomen voor verder gebruik in de analyse
+    Perc_symptomen = approx(
+      x = Date_of_statistics,
+      y = Perc_covid_symptoms,
+      xout = Date_of_statistics)$y) %>%
+  # Bereken gemiddelde percentage symptomen per week
   group_by(
     Week) %>%
   summarise(
-    n_symptomen = total_cases %>% sum(na.rm = TRUE),
-    n_personen = total_rep %>% sum(na.rm = TRUE)) %>%
-  mutate(
-    Perc_symptomen = n_symptomen/n_personen)
+    n_dagen = n(),
+    Perc_symptomen = Perc_symptomen %>% mean()) %>%
+  # Maar alleen voor volledige weken
+  filter(
+    n_dagen == 7) %>%
+  select(
+    -n_dagen)
