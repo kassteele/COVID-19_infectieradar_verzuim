4712efdf93532b2800dca5525e9bbde8fb230fe0
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2022-09-21 09:58:09 +0200

    Update outpout bestandsnaam

--- Scripts/07_exporteer_figuren.R
+++ Scripts/07_exporteer_figuren.R
@@ -1,8 +1,8 @@
 #
-# Exporteren figuren
+# Exporteer figuren
 #
 
 ggsave(
   plot = plot_verzuim_tijdreeks_fit,
-  filename = str_glue("Resultaten/Ziekteverzuim_bedrijfstak_tijdreeks_fit_tm{max(data_verzuim$Week)}.png"),
-  width = 1920, height = 1080, units = "px", dpi = 100, bg = "white")
+  filename = str_glue("Output/Infectieradar_verzuim_{format(max_week, '%Y%m%d')}.png"),
+  width = 1920, height = 1080, units = "px", dpi = 120, bg = "white")
